21bb8b5d4e91fc497d8d19319777a1934ea85ac8
Sofiane Belaiche <62851627+belaiche-sofiane@users.noreply.github.com>
2020-12-30 11:18:18 +0100

    Delete ConnexionClient.sh

--- ConnexionClient.sh
+++
@@ -1,1 +0,0 @@
-curl --header "Content-type:application/json" -X POST --data '{"nom":"Sonsuper", "prénom":"Alexis","email:":"alexis.sonsuper@mailserver.fr","password":"123"}' localhost:8888/membre/connexion
