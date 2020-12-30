5a7fc1b9b85f0d4780eedf34c5ad3fbf0abebd0b
Sofiane Belaiche <62851627+belaiche-sofiane@users.noreply.github.com>
2020-12-30 11:18:51 +0100

    Delete CreationBase.sh

--- CreationBase.sh
+++
@@ -1,4 +0,0 @@
-mongoimport --db ecommerceDatabase --collection produits --file Produits.json --jsonArray --drop
-mongoimport --db ecommerceDatabase --collection membres --file membres.json --jsonArray --drop
-mongoimport --db ecommerceDatabase --collection achat --file panier.json --jsonArray --drop
-
